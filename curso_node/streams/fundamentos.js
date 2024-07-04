00afe4c6b9a5fd0b9678eb876459a329fa854e37
naralicecosta <naralicecosta04@gmail.com>
2024-07-03 21:35:57 -0300

    docs: :memo: métodos de stream

--- curso_node/streams/fundamentos.js
+++ curso_node/streams/fundamentos.js
@@ -3,7 +3,7 @@
 // //tudo que estou recebendo como entrado, estou encaminhando para uma saida
 
 //streams - consigo trabalhar com os dados antes deles estarem completos
-import { Readable} from 'node:stream'
+import { Readable, Writable, Transform} from 'node:stream'
 
 class OnetoHundredStream extends Readable {
     index = 1
@@ -22,4 +22,24 @@ class OnetoHundredStream extends Readable {
         
     }
 }
-new OnetoHundredStream().pipe(process.stdout) //enquanto leio a stream, ele ja executa no terminal
+class InverseNumberStream extends Transform{
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+        callback(null, Buffer.from(String(transformed)))
+
+    }
+}
+
+class MultiplyByTenStream extends Writable{
+    //o que é enviado na strram de leitura, conseguimos ler o chunk dentro da stream de escrita, como a informaçãoe esta codififcada, função que a tream de scrita precisa chamar quando ela terminou de fazer a execução
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback() //encerra tudo que precisava ser executado
+
+    }
+}
+
+
+new OnetoHundredStream().pipe(new InverseNumberStream())
+                        .pipe(new MultiplyByTenStream()) //enquanto leio a stream, ele ja executa no terminal
+//stream de escrita que processa dados, mas não le dados
