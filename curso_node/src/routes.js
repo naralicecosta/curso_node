872e468167a316976b4a73f973dc92fdc3002205
naralicecosta <naralicecosta04@gmail.com>
2024-07-08 22:23:40 -0300

    feat: :sparkles: criando método delete

--- curso_node/src/routes.js
+++ curso_node/src/routes.js
@@ -37,9 +37,32 @@ export const routes = [
         }
     },
     {
-        method: 'DELETE',
-        path: buildRoutePath( '/users/:id/groups/:groupId'),
+        method: 'PUT',
+        path: buildRoutePath( '/users/:id'),
         handler: (request, response) =>{
+            const {id} = request.params
+            const {name, email,} = request.body
+
+            database.update('users', id,{
+                name,
+                email,
+
+            })
+
+            return response.writeHead(204).end();
+
+        }
+    },
+
+    {
+        method: 'DELETE',
+        path: buildRoutePath( '/users/:id'),
+        handler: (request, response) =>{
+            const {id} = request.params
+
+            database.delete('users', id)
+
+            return response.writeHead(204).end();
 
         }
     }
